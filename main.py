51ba90f5b6c5ebe12ee4ffc444a5e68d505ea486
K3therLOL <g3roylol@gmail.com>
2026-07-12 04:36:17 +0300

    deleted comments and output of needed info started

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import pandas as pd
 
 def main():
     df = pd.read_excel("./credit_data.xlsx")
-    print(df.head())
-    #print(df.tail())
-    #print(df.info())
+    print(f"Dimensions:\n\n{df.ndim}")
+    print("-------------------------")
+    print(f"First 5 lines:\n\n{df.head()}")
+    print("-------------------------")
+    print(f"First 5 lines:\n\n{df.tail()}")
 
 if __name__ == "__main__":
     main()
