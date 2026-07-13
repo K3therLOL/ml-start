ae7109326aed4aae8ccb4d44034e21e77bd46b99
K3therLOL <g3roylol@gmail.com>
2026-07-13 20:06:54 +0300

    need_to_work_better exercise completed

---
+++ extra/07_need_to_work_better_.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+def need_to_work_better(journal: pd.DataFrame) -> pd.DataFrame:
+    return journal.loc[(journal.iloc[:, 1:] == 2).any(axis=1)]
+
+columns = ['name', 'maths', 'physics', 'computer science']
+data = {
+    'name': ['Иванов', 'Петров', 'Сидоров', 'Васечкин', 'Николаев'],
+    'maths': [5, 4, 5, 2, 4],
+    'physics': [4, 4, 4, 5, 5],
+    'computer science': [5, 2, 5, 4, 3]
+}
+journal = pd.DataFrame(data, columns=columns)
+filtered = need_to_work_better(journal)
+print(journal)
+print(filtered)
